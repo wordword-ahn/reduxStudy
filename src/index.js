bd471842139948e063be55d7d91a60b05ab304f4
wordword <ggavi2000@naver.com>
2020-06-30 15:34:09 +0900

    6. 버튼에 action 연결

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-/* 
-[전체 흐름]
-1. Store : Store를 create 한다.
-2. reducer : '유일하게' data를 바꾸고 수정할 수 있는 함수다. 들어온 action에 따라 값을 변경한다.
-3. action : dispatch를 통해 reducer에게 'data를 50으로 바꾸세요!' 이런 식으로 지시를 내린다.
-*/
-
-
 import { createStore } from "redux";  // store가 하는 일: 나의 data(count)를 넣을 수 있는 장소를 생성
-
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
 
 
 // [1] reducer  ('유일하게' data를 바꾸고 수정할 수 있는 함수)
-const reducer = (count = 0, action) => {     // state = 0 : default 값 지정 (안그러면 최초 상태가 undefined로 뜬다)
+const reducer = (count = 0, action) => {     // state = 0  (default 값 지정: 안그러면 최초 상태가 undefined로 뜬다)
 
-  // state 변경 (state 변경은 action을 통해 가능하다) (action: reducer와 소통하기 위한 방법)
+  // state 변경 (state 변경은 action을 통해 가능하다)
   if(action.type === "ADD") {
     return count + 1;
   }
@@ -31,27 +19,25 @@ const reducer = (count = 0, action) => {     // state = 0 : default 값 지정 (
 };
 
 
-
 // [2] Store
 const countStore = createStore(reducer);
 
 
-// [3] Action
-countStore.dispatch({type: "ADD"});  // 우리가 reducer에게 "ADD 해주세요!!"라고 요청하려면 action에 ADD를 넣어서 reducer에게 전달해야 한다. action은 object 형태여야 한다.
-countStore.dispatch({type: "ADD"});  // 우리가 reducer에게 "ADD 해주세요!!"라고 요청하려면 action에 ADD를 넣어서 reducer에게 전달해야 한다. action은 object 형태여야 한다.
-countStore.dispatch({type: "ADD"});  // 우리가 reducer에게 "ADD 해주세요!!"라고 요청하려면 action에 ADD를 넣어서 reducer에게 전달해야 한다. action은 object 형태여야 한다.
-countStore.dispatch({type: "ADD"});  // 우리가 reducer에게 "ADD 해주세요!!"라고 요청하려면 action에 ADD를 넣어서 reducer에게 전달해야 한다. action은 object 형태여야 한다.
-countStore.dispatch({type: "ADD"});  // 우리가 reducer에게 "ADD 해주세요!!"라고 요청하려면 action에 ADD를 넣어서 reducer에게 전달해야 한다. action은 object 형태여야 한다.
-countStore.dispatch({type: "MINUS"});  // 우리가 reducer에게 "ADD 해주세요!!"라고 요청하려면 action에 ADD를 넣어서 reducer에게 전달해야 한다. action은 object 형태여야 한다.
-
-
-// [4] ADD라는 액션이 끝난 뒤 Store에 들어간 데이터 확인
-console.log(countStore.getState());
 
 
 
+// [버튼을 만들기 위한 작업]
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
+// 버튼에 전달할 함수
+const handleAdd = () => { countStore.dispatch({ type: "ADD" }) }
+const handleMinus = () => { countStore.dispatch({ type: "MINUS" })}
 
+// 버튼에 이벤트 연결
+add.addEventListener("click", handleAdd)      // add 버튼을 누르면 ADD라는 액션이 reducer에 전달된다!
+minus.addEventListener("click", handleMinus)  // add 버튼을 누르면 ADD라는 액션이 reducer에 전달된다!
 
 
 
