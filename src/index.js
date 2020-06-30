b49e5a5d2773198e489a217e1f84a6f769ffa7d5
wordword <ggavi2000@naver.com>
2020-06-30 10:58:55 +0900

    1. 리덕스 변환하기 전 기본상태 (더하기 빼기 버튼만 작동되게)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
+// [1]
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+
+
+// [2-1] 변경된 값 텍스트에 반영하기
+let count = 0;
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+}
+
+
+
+// [2-2]
 const handleAdd = () => {
-  console.log("더함");
+  count = count + 1;
+  updateText();  // 이걸 해줘야 변경된 값이 텍스트에 반영된다.
 }
 
 const handleMinus = () => {
-  console.log("빼기");
+  count = count - 1;
+  updateText();  // 이걸 해줘야 변경된 값이 텍스트에 반영된다.
 }
 
+
+
+// [3]
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
