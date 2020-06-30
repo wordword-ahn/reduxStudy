68b14b4af02c8f54108cb6e58defd21034f30263
wordword <ggavi2000@naver.com>
2020-06-30 17:48:20 +0900

    9. subscribe를 통한 데이터 변화 확인

--- src/index.js
+++ src/index.js
@@ -10,11 +10,20 @@ const DELETE_TODO = "DELETE_TODO";
 const reducer = (state = [], action) => {
   console.log(action);
 
-  // state 변경 (state 변경은 action을 통해 가능하다)
-  switch (action.type) {    
-    case ADD_TODO    : return []
-    case DELETE_TODO : return []
-    default          : return state;
+  // state 변경
+  // 1. 항상 새로운 state를 create하고 그 새로운 state를 return한다.
+  // 2. 절대 old state의 값을 리턴하면 안된다.  예) return state.push(action.text);
+
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];  // 삭제를 위해 id 값도 추가한다.
+   // return [{ text: action.text, id: Date.now() }, ...state];  // 참고: 이렇게 할 경우 최근에 입력된 값이 최상단에 올라간다
+
+    case DELETE_TODO:
+      return [];
+
+    default:
+      return state;
   }
 };
 
@@ -25,18 +34,26 @@ const store = createStore(reducer);
 
 
 
-// [3] dispatch를 통해 reducer에 action을 전달하기
+// [3] dispatch를 통해 reducer에 action 전달
 const onSubmit = (e) => {
   e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
+  const toDo = input.value;  // form이 submit 되면 input에서 값을 가져온다.
+  input.value = "";          // input 창은 다시 깨끗하게 비워주자.
 
+  // 'input 값을 추가해주세요'라는 action을 전달하면서 input에서 받은 텍스트도 함께 넘겨준다.
   store.dispatch({type: ADD_TODO, text: toDo})
 }
 
 
 
-// [4] index.html 연결 (버튼 이벤트 연결)
+// [4] subscribe (store에 변화가 생길 때마다 발동)
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+
+
+// [5] index.html 연결 (버튼 이벤트 연결)
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
