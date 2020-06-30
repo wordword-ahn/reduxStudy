2869f1fa7db4772148645616372c781a0af5e50a
wordword <ggavi2000@naver.com>
2020-06-30 19:27:57 +0900

    11. 삭제 버튼 추가 (id를 통한 타겟 삭제는 미구현)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import { createStore } from "redux";
 
-
 // 오타로 인한 오작동을 막기 위한 선언
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
 
+// 추가 버튼 누르면 발동되는 이벤트
+const dispatchAddToDo = (text) => { 
+  store.dispatch(addToDo(text)) 
+}
+
+const addToDo = (text) => { 
+  return { 
+    type: ADD_TODO,  // action 타입
+    text             // 함께 전달
+  } 
+}
+
+
+// 삭제 버튼 누르면 발동되는 이벤트
+const dispatchDeleteToDo = (e) => { 
+  const id = e.target.parentNode.id;  // 처음에 데이터 추가할 때 reducer에서 넣었었던 id를 알아낸다.
+  store.dispatch(deleteToDo(id));
+}
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,  // action 타입
+    id                  // 함께 전달
+  }
+}
+
+
+
+
+
 // [1] reducer  ('유일하게' data를 바꾸고 수정할 수 있는 함수)
 const reducer = (state = [], action) => {
-  console.log(action);
 
   // state 변경 (항상 새로운 state를 create하고 그 새로운 state를 return 한다)
   switch (action.type) {
@@ -24,25 +52,18 @@ const reducer = (state = [], action) => {
 };
 
 
-
 // [2] Store
 const store = createStore(reducer);
 
 
-
 // [3] dispatch를 통해 reducer에 action 전달
-const addToDo = (text) => {
-  store.dispatch({ type: ADD_TODO, text })  // 'input 값을 추가해주세요'라는 action을 전달하면서 input에서 받은 텍스트도 함께 넘겨준다.
-}
-
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;  // form이 submit 되면 input에서 값을 가져온다.
   input.value = "";          // input 창은 다시 깨끗하게 비워주자.
 
-  addToDo(toDo);
+  dispatchAddToDo(toDo);
 }
-
 
 
 // [4] subscribe (store에 변화가 생길 때마다 화면을 갱신한다)
@@ -59,10 +80,18 @@ const paintToDos = () => {
 
   // 스토어에서 가져온 데이터의 갯수만큼 반복문을 돌면서 li 태그를 계속 생성한다.
   toDos.forEach(toDo => {
+    
+    // 삭제 버튼 추가
+    const btn = document.createElement("button");
+    btn.addEventListener("click", dispatchDeleteToDo);  // 삭제 버튼 이벤트
+    btn.innerText = "삭제";
+
     const li = document.createElement("li");
     li.id = toDo.id;           // index.html의 li 태그 안에 id를 준다 (참고: 리듀서에서 모든 데이터에 id를 부여했음)
     li.innerText = toDo.text;  // innerText: DOM 요소 내의 내용을 조작한다.
+
     ul.appendChild(li);        // ul태그 안에 (위에서 텍스트를 넣어준) li 태그를 추가해준다.
+    li.appendChild(btn);       // li태그 안에 삭제 버튼 추가
   })
 }
 
