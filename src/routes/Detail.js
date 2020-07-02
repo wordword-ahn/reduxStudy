cd1d7601709dbda98c734ef678a17bd57f31b7ad
wordword <ggavi2000@naver.com>
2020-07-02 14:45:06 +0900

    21. useState 여러개 하나로 묶기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 
-function Detail({ toDo }) {    
+function Detail({ information }) {    
     
     return (
         <>
             {/* 콘솔 로그로 toDo 찍어보면 toDo -> text 순으로 값이 있다. 근데 toDo 안이 텅 비어있다면 오류 떠서 예외처리 필요 */}
-            <h1> { toDo && toDo.이름 ? toDo.이름 : "삭제된 페이지랍니다"} </h1> <hr></hr>
+            <h1> { information && information.이름 ? information.이름 : "삭제된 페이지랍니다"} </h1> <hr></hr>
 
             <h2> 기본 정보 </h2>
-            <h5> 아이디: {toDo && toDo.id ? toDo.id : "정보 없음"} </h5>
-            <h5> 휴대번화번호: {toDo && toDo.휴대전화번호 ? toDo.휴대전화번호 : ""} </h5>
+            <h5> 아이디(key): {information && information.id ? information.id : "정보 없음"} </h5>
+            <h5> 휴대번화번호: {information && information.휴대전화번호 ? information.휴대전화번호 : ""} </h5>
         </>)
 }
 
 function mapStateToProps(state, ownProps) {
     const id = ownProps.match.params.id;    // console.log(ownProps) 이걸로 확인해보면 match -> params -> id가 존재한다는 것을 알 수 있다.    
-    return { toDo: state.find(toDo => toDo.id === parseInt(id))}  // 배열.find() 함수는 조건에 만족하는 첫번째 요소를 반환한다.
+    return { information: state.find(information => information.id === parseInt(id))}  // 배열.find() 함수는 조건에 만족하는 첫번째 요소를 반환한다.
 }
 
 export default connect(mapStateToProps, null)(Detail);
--- src/routes/SavePhone.js
+++ src/routes/SavePhone.js
@@ -1,32 +1,68 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-function SavePhone({ addToDo }) {
+// movePage: 제출 버튼 누른뒤 메인화면으로 페이지 이동되게 하려고 추가함
+function SavePhone({ addToDo, movePage }) {
     
-    // 사용자가 입력한 텍스트로 변경
-    const [이름, setText1] = useState("");
-    const [휴대전화번호, setText2] = useState("");
+    /*
+        // 기존방식: useState랑 onChange를 무식하게 여러개 만들어서 구현
+        const [text1, setText1] = useState("");
+        const [text2, setText2] = useState("");
 
-    function onChange1(e) { setText1(e.target.value) }
-    function onChange2(e) { setText2(e.target.value) }
+        function onChange1(e) { setText1(e.target.value) }
+        function onChange2(e) { setText2(e.target.value) }
+    */
+
+    // 수정
+    const [inputs, setInputs] = useState({
+        이름: "",
+        휴대전화번호: "",
+    });
+
+    const { 이름, 휴대전화번호 } = inputs; // 비구조화 할당을 통해 값 추출
+
+    const onChange = (e) => {
+        setInputs({
+            ...inputs,  // 기존의 inputs 객체를 복사한 뒤
+            [e.target.name]: e.target.value  // name 키를 가진 값을 value로 설정
+        });
+    };
 
     // 버튼 누르면 이게 발동
     function onSubmit(e) {
-        e.preventDefault();
+
+        // 예외처리
+        if(이름.length <= 0) {
+            e.preventDefault();  // 이거 없으면 입력한 값 싹다 날아감
+            alert( '이름은 필수입력 항목입니다.' );
+            return;
+        }
+
+        e.preventDefault();  
         addToDo(이름, 휴대전화번호);
-        setText1("");
-        setText2("");
+
+        // 제출후 초기화
+        setInputs({
+            이름: "",
+            휴대전화번호: ""
+        });
+
+        // 히스토리: 제출 버튼 누른뒤 메인화면으로 페이지 이동되게 하려고 추가함
+        movePage.history.push("/");
     }
 
     return (
         <>
             <h1> 기본정보 </h1>
             <form onSubmit={onSubmit}>
-                <input type="text" value={이름} onChange={onChange1} />
-                <input type="text" value={휴대전화번호} onChange={onChange2} />
 
-                <button> 연락처 등록 </button>
+                이름:       <input type="text" name="이름" value={이름} onChange={onChange} /> <br></br>
+                휴대전화번호: <input type="tel" name="휴대전화번호" value={휴대전화번호} onChange={onChange} /> <br></br>
+
+                <button> <Link to="/"> 취소 </Link> </button>
+                <button> 등록 </button>
             </form>
         </>)
 }
@@ -40,7 +76,8 @@ function mapDispatchToProps(dispatch, ownProps) {
 
     return {
         // 아래의 값을 리턴한 뒤, 콘솔로그로 Home.js에 들어오는 addToDo를 확인해보면 addToDo라는 함수가 다시 들어온다.
-        addToDo: (이름, 휴대전화번호) => dispatch(actionCreators.addToDo(이름, 휴대전화번호))   // dispatch를 호출 -> 그리고 dispatch는 store에서 만든 actionCreators를 호출 -> actionCreators 안에 있는 여러 action들 중에서 addToDo를 가져옴 -> addToDo는 텍스트를 인자로 받음
+        addToDo: (이름, 휴대전화번호) => dispatch(actionCreators.addToDo(이름, 휴대전화번호)),  // dispatch를 호출 -> 그리고 dispatch는 store에서 만든 actionCreators를 호출 -> actionCreators 안에 있는 여러 action들 중에서 addToDo를 가져옴 -> addToDo는 텍스트를 인자로 받음
+        movePage: ownProps  // 제출후 페이지 이동시키려고 추가
     };
 }
 
